029b3853986e6f942ca2de3def4565f9aa7974b6
Eduardo Ervideira <ejcept@gmail.com>
2025-01-30 19:17:12 +0000

    refactor: restructure main loop and add input handling

--- src/main.c
+++ src/main.c
@@ -2,38 +2,42 @@
 #include <stdlib.h>
 #include <signal.h>
 #include <unistd.h>
-
-void loop(void){
-    char *input;
-    char *prompt = "gsh> ";
-    size_t bufsize = 0;
-
-    while(1){
-        printf("%s", prompt);
-        if (getline(&input, &bufsize, stdin) == -1) {
-            printf("\n");
-            break;
-        }
-        
-        printf("received: %s", input);
-        if(!strcmp(input, "exit\n")){
-            break;
-        }
-
-        free(input);
-        input = NULL;
-    }
-}
+#include <string.h>
+#include <stdbool.h>
+#include "terminal.h"
+#include "input.h"
 
 int main(int argc, char **argv){
-    signal(SIGINT, SIG_IGN);
+    //signal(SIGINT, SIG_IGN);
+    struct termios old_termios;
+    set_raw_mode(&old_termios);
 
     if(argc > 1){
         fprintf(stderr, "gsh can't execute scripts (yet)\n");
         exit(EXIT_FAILURE);
     } else {
-        loop();
+        bool running = true;
+        char *prompt = "gsh> ";
+        char input[MAX_INPUT_LENGTH];
+        size_t cursor_pos = 0;
+        size_t input_length = 0;
+
+        while (running) {
+            get_input(input, &cursor_pos, &input_length); // check error code?
+
+            if (!strcmp(input, "exit")) {
+                running = false;
+            } else {
+                printf("\n");
+                printf("< %s\n", input);
+                memset(input, 0, MAX_INPUT_LENGTH);
+                cursor_pos = 0;
+                input_length = 0;
+            }
+        }
     }
+
+    reset_terminal(&old_termios);
 
     return 0;
 }
