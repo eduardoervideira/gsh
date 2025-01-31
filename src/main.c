f8e91e2ddb0a1d8157d01f5450f17ec53e6f3baf
Eduardo Ervideira <ejcept@gmail.com>
2025-01-31 12:37:08 +0000

    feat: add tokenizer for input processing

--- src/main.c
+++ src/main.c
@@ -6,6 +6,8 @@
 #include <stdbool.h>
 #include "terminal.h"
 #include "input.h"
+#include "tokenizer.h"
+#include "executor.h"
 
 int main(int argc, char **argv){
     //signal(SIGINT, SIG_IGN);
@@ -16,6 +18,7 @@ int main(int argc, char **argv){
         fprintf(stderr, "gsh can't execute scripts (yet)\n");
         exit(EXIT_FAILURE);
     } else {
+        printf("\033[2J\033[H");
         bool running = true;
         char *prompt = "gsh> ";
         char input[MAX_INPUT_LENGTH];
@@ -23,21 +26,34 @@ int main(int argc, char **argv){
         size_t input_length = 0;
 
         while (running) {
+            // clean all the terminal before
             get_input(input, &cursor_pos, &input_length); // check error code?
+            printf("\n");
 
-            if (!strcmp(input, "exit")) {
+            if(!strcmp(input, "exit")){
                 running = false;
             } else {
-                printf("\n");
-                printf("< %s\n", input);
-                memset(input, 0, MAX_INPUT_LENGTH);
-                cursor_pos = 0;
-                input_length = 0;
+                Token *tokens = NULL;
+                int num_tokens = tokenize(input, &tokens);
+
+                if(num_tokens > 0){
+                    execute(tokens, num_tokens);
+
+                    /*for (int i = 0; i < num_tokens; i++){
+                        printf("  [%s] -> ", tokens[i].value);
+                        print_token(tokens[i].type);
+                        printf("\n");
+                    }*/
+                }
+
+                for(int i = 0; i < num_tokens; i++)
+                    free(tokens[i].value);
+
+                free(tokens);
             }
         }
     }
 
     reset_terminal(&old_termios);
-
     return 0;
 }
